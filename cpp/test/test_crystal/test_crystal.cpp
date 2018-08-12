a5554c6884c6638c68bb7d64e012971e3f6ec637
zhangjiajie <zhangjiajie@kuaishou.com>
2018-08-12 21:30:09 +0800

    gtest file structure

---
+++ cpp/test/test_crystal/test_crystal.cpp
@@ -0,0 +1,25 @@
+// #include "mockbar.h"
+#include "test_crystal.h"
+
+using ::testing::Return;
+
+CrystalTest()
+{
+}
+
+// TEST_F(CrystalTest, ByDefaultBazTrueIsTrue) {
+//     Foo foo(m_bar);
+//     EXPECT_EQ(foo.baz(true), true);
+// }
+
+// TEST_F(CrystalTest, ByDefaultBazFalseIsFalse) {
+//     Foo foo(m_bar);
+//     EXPECT_EQ(foo.baz(false), false);
+// }
+
+// TEST_F(CrystalTest, SometimesBazFalseIsTrue) {
+//     Foo foo(m_bar);
+//     // Have norf return true for once
+//     EXPECT_CALL(m_bar,norf()).WillOnce(Return(true));
+//     EXPECT_EQ(foo.baz(false), false);
+// }
