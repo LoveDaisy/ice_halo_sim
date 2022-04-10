1a2e55893b0df1a4f0e6d87699263121c330842a
Jiajie Zhang <zhangjiajie043@gmail.com>
2022-04-10 22:17:17 +0800

    add expected out for test v3

--- cpp/test/test_v3.cpp
+++ cpp/test/test_v3.cpp
@@ -2,6 +2,7 @@
 
 #include <chrono>
 #include <cstddef>
+#include <cstring>
 #include <memory>
 #include <thread>
 
@@ -43,8 +44,12 @@ TEST_F(V3Test, Simple) {
   config_queue->Emplace(std::move(config));
 
   v3::Simulator simulator(config_queue, data_queue);
+  std::unique_ptr<float[]> output_data{ new float[12 * 7]{} };
+  float* output_data_ptr = output_data.get();
+
   std::thread producer([&simulator]() { simulator.Run(); });
   std::thread consumer([=]() {
+    int offset = 0;
     while (true) {
       auto data = data_queue->Get();
       if (!data || data->Empty()) {
@@ -54,6 +59,10 @@ TEST_F(V3Test, Simple) {
       for (size_t i = 0; i < data->size_; i++) {
         LOG_DEBUG("%.6f,%.6f,%.6f  %.6f,%.6f,%.6f  %.6f", data->p()[i * 3 + 0], data->p()[i * 3 + 1],
                   data->p()[i * 3 + 2], data->d()[i * 3 + 0], data->d()[i * 3 + 1], data->d()[i * 3 + 2], data->w()[i]);
+        std::memcpy(output_data_ptr + offset * 7 + 0, data->p() + i * 3, 3 * sizeof(float));
+        std::memcpy(output_data_ptr + offset * 7 + 3, data->d() + i * 3, 3 * sizeof(float));
+        std::memcpy(output_data_ptr + offset * 7 + 6, data->w() + i * 1, 1 * sizeof(float));
+        offset++;
       }
     }
   });
@@ -63,6 +72,27 @@ TEST_F(V3Test, Simple) {
 
   producer.join();
   consumer.join();
+
+  float expect_out[11 * 7]{
+    /* --------- p --------------->|<-------------- d ------------->|<-- w -->|*/
+    0.302099,  0.040812,  0.150000,  -0.939667, 0.003329,  0.342074,  0.127290,  // data 1,1
+    0.165777,  -0.404289, -0.079173, -0.467876, -0.814561, -0.342904, 0.063834,  // data 1,2
+    -0.006257, 0.041904,  -0.150000, -0.939667, 0.003329,  -0.342074, 0.761623,  // data 2,1
+    -0.314613, 0.042997,  0.150000,  -0.939667, 0.003329,  0.342074,  0.096947,  // data 3,1
+    -0.433013, -0.185722, -0.048089, -0.833166, 0.433879,  0.342904,  0.917659,  // data 3,2
+    -0.433013, 0.043416,  0.034809,  -0.405241, 0.003329,  -0.914204, 0.012868,  // data 4,1
+    -0.243057, 0.044089,  -0.150000, 0.939667,  0.003329,  -0.342074, 0.001111,  // data 5,1
+    0.433013,  0.130390,  0.098259,  0.833166,  0.433879,  -0.342904, 0.018141,  // data 5,2
+    0.065299,  0.045181,  0.150000,  0.939667,  0.003329,  0.342074,  0.000141,  // data 6,1
+    -0.224493, 0.370389,  -0.091418, -0.939368, 0.002088,  -0.342904, 0.000343,  // data 6,2
+    0.373655,  0.046274,  -0.150000, 0.939667,  0.003329,  -0.342074, 0.000018,  // data 7,1
+  };
+
+  for (int i = 0; i < 11; i++) {
+    for (int j = 0; j < 7; j++) {
+      EXPECT_NEAR(expect_out[i * 7 + j], output_data[i * 7 + j], 1e-5);
+    }
+  }
 }
 
 }  // namespace
