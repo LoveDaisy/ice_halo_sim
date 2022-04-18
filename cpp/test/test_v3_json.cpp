13c08e24fbcfe6c65b12531eb1527be631a69c44
Jiajie Zhang <zhangjiajie043@gmail.com>
2022-04-18 20:15:32 +0800

    update test

--- cpp/test/test_v3_json.cpp
+++ cpp/test/test_v3_json.cpp
@@ -1,6 +1,7 @@
 #include <gtest/gtest.h>
 
 #include <fstream>
+#include <utility>
 #include <variant>
 
 #include "core/math.hpp"
@@ -8,6 +9,7 @@
 #include "protocol/crystal_config.hpp"
 
 extern std::string config_file_name;
+using namespace icehalo;
 
 namespace {
 
@@ -23,18 +25,29 @@ class V3TestJson : public ::testing::Test {
 
 TEST_F(V3TestJson, Distribution) {
   const auto& j_height = config_json_.at("crystal")[1].at("shape").at("height");
-  auto d = j_height.get<icehalo::Distribution>();
+  auto d = j_height.get<Distribution>();
 
-  ASSERT_EQ(d.type, icehalo::DistributionType::kGaussian);
+  ASSERT_EQ(d.type, DistributionType::kGaussian);
   ASSERT_NEAR(d.mean, 1.3, 1e-5);
   ASSERT_NEAR(d.std, 0.2, 1e-5);
 }
 
 TEST_F(V3TestJson, Crystal) {
-	const auto& j_crystal = config_json_.at("crystal")[1];
-	auto c = j_crystal.get<icehalo::v3::CrystalConfig>();
+  const auto& j_crystal = config_json_.at("crystal")[1];
+  auto c = j_crystal.get<v3::CrystalConfig>();
 
-	ASSERT_EQ(c.id_, 2);
+  ASSERT_EQ(c.id_, 2);
+  ASSERT_TRUE(std::holds_alternative<v3::PrismCrystalParam>(c.param_));
+
+  const auto& p = std::get<v3::PrismCrystalParam>(c.param_);
+  ASSERT_EQ(p.h_.type, DistributionType::kGaussian);
+  ASSERT_NEAR(p.h_.mean, 1.3, 1e-5);
+  ASSERT_NEAR(p.h_.std, 0.2, 1e-5);
+  for (const auto& x : p.d_) {
+    ASSERT_EQ(x.type, DistributionType::kNoRandom);
+    ASSERT_NEAR(x.mean, 1.0f, 1e-5);
+    ASSERT_NEAR(x.std, 0.0f, 1e-5);
+  }
 }
 
 }  // namespace
