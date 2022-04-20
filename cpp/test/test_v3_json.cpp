778ce7c0d0febd93630805f07af926b1b714c4a0
Jiajie Zhang <zhangjiajie043@gmail.com>
2022-04-21 00:16:54 +0800

    add pyramid test

--- cpp/test/test_v3_json.cpp
+++ cpp/test/test_v3_json.cpp
@@ -24,13 +24,18 @@ class V3TestJson : public ::testing::Test {
   nlohmann::json config_json_;
 };
 
+#define CHECK_DISTRIBUTION(d, t, m, s) \
+  do {                                 \
+    ASSERT_EQ(d.type, t);              \
+    ASSERT_NEAR(d.mean, m, 1e-5);      \
+    ASSERT_NEAR(d.std, s, 1e-5);       \
+  } while (false)
+
 TEST_F(V3TestJson, Distribution) {
   const auto& j_height = config_json_.at("crystal")[1].at("shape").at("height");
   auto d = j_height.get<Distribution>();
 
-  ASSERT_EQ(d.type, DistributionType::kGaussian);
-  ASSERT_NEAR(d.mean, 1.3, 1e-5);
-  ASSERT_NEAR(d.std, 0.2, 1e-5);
+  CHECK_DISTRIBUTION(d, DistributionType::kGaussian, 1.3, 0.2);
 }
 
 TEST_F(V3TestJson, Crystal_PrismSimple) {
@@ -41,13 +46,9 @@ TEST_F(V3TestJson, Crystal_PrismSimple) {
   ASSERT_TRUE(std::holds_alternative<v3::PrismCrystalParam>(c.param_));
 
   const auto& p = std::get<v3::PrismCrystalParam>(c.param_);
-  ASSERT_EQ(p.h_.type, DistributionType::kGaussian);
-  ASSERT_NEAR(p.h_.mean, 1.3, 1e-5);
-  ASSERT_NEAR(p.h_.std, 0.2, 1e-5);
+  CHECK_DISTRIBUTION(p.h_, DistributionType::kGaussian, 1.3, 0.2);
   for (const auto& x : p.d_) {
-    ASSERT_EQ(x.type, DistributionType::kNoRandom);
-    ASSERT_NEAR(x.mean, 1.0f * math::kSqrt3_4, 1e-5);
-    ASSERT_NEAR(x.std, 0.0f, 1e-5);
+    CHECK_DISTRIBUTION(x, DistributionType::kNoRandom, 1.0f * math::kSqrt3_4, 0.0f);
   }
 }
 
@@ -59,25 +60,45 @@ TEST_F(V3TestJson, Crystal_PrismFull) {
   ASSERT_TRUE(std::holds_alternative<v3::PrismCrystalParam>(c.param_));
 
   const auto& p = std::get<v3::PrismCrystalParam>(c.param_);
-  ASSERT_EQ(p.h_.type, DistributionType::kUniform);
-  ASSERT_NEAR(p.h_.mean, 1.2, 1e-5);
-  ASSERT_NEAR(p.h_.std, 0.4, 1e-5);
+  CHECK_DISTRIBUTION(p.h_, DistributionType::kUniform, 1.2f, 0.4f);
   for (const auto& x : p.d_) {
-    ASSERT_EQ(x.type, DistributionType::kGaussian);
-    ASSERT_NEAR(x.mean, 1.0f * math::kSqrt3_4, 1e-5);
-    ASSERT_NEAR(x.std, 0.2f * math::kSqrt3_4, 1e-5);
+    CHECK_DISTRIBUTION(x, DistributionType::kGaussian, 1.0f * math::kSqrt3_4, 0.2f * math::kSqrt3_4);
   }
 
   const auto& axis = c.axis_;
-  ASSERT_EQ(axis.azimuth_dist.type, DistributionType::kUniform);
-  ASSERT_NEAR(axis.azimuth_dist.mean, 0, 1e-5);
-  ASSERT_NEAR(axis.azimuth_dist.std, 360, 1e-5);
-  ASSERT_EQ(axis.latitude_dist.type, DistributionType::kGaussian);
-  ASSERT_NEAR(axis.latitude_dist.mean, 90, 1e-5);
-  ASSERT_NEAR(axis.latitude_dist.std, 1.2, 1e-5);
-  ASSERT_EQ(axis.roll_dist.type, DistributionType::kUniform);
-  ASSERT_NEAR(axis.roll_dist.mean, 0, 1e-5);
-  ASSERT_NEAR(axis.roll_dist.std, 360, 1e-5);
+  CHECK_DISTRIBUTION(axis.azimuth_dist, DistributionType::kUniform, 0, 360);
+  CHECK_DISTRIBUTION(axis.latitude_dist, DistributionType::kGaussian, 90, 1.2);
+  CHECK_DISTRIBUTION(axis.azimuth_dist, DistributionType::kUniform, 0, 360);
+}
+
+TEST_F(V3TestJson, Pyramid_Simple) {
+  const auto& j_crystal = config_json_.at("crystal")[4];
+  auto c = j_crystal.get<v3::CrystalConfig>();
+
+  ASSERT_EQ(c.id_, 5);
+  ASSERT_TRUE(std::holds_alternative<v3::PyramidCrystalParam>(c.param_));
+
+  const auto& p = std::get<v3::PyramidCrystalParam>(c.param_);
+  CHECK_DISTRIBUTION(p.h_pyr_u_, DistributionType::kNoRandom, 0.1, 0);
+  CHECK_DISTRIBUTION(p.h_pyr_l_, DistributionType::kNoRandom, 0.5, 0);
+  CHECK_DISTRIBUTION(p.h_prs_, DistributionType::kNoRandom, 1.2, 0);
+
+  ASSERT_EQ(p.miller_indices_u_[0], 2);
+  ASSERT_EQ(p.miller_indices_u_[1], 0);
+  ASSERT_EQ(p.miller_indices_u_[2], 3);
+
+  ASSERT_EQ(p.miller_indices_l_[0], 1);
+  ASSERT_EQ(p.miller_indices_l_[1], 0);
+  ASSERT_EQ(p.miller_indices_l_[2], 1);
+
+  for (const auto& x : p.d_) {
+    CHECK_DISTRIBUTION(x, DistributionType::kNoRandom, 1.0f * math::kSqrt3_4, 0.0f);
+  }
+
+  const auto& axis = c.axis_;
+  CHECK_DISTRIBUTION(axis.azimuth_dist, DistributionType::kUniform, 0, 360);
+  CHECK_DISTRIBUTION(axis.latitude_dist, DistributionType::kNoRandom, 90, 0);
+  CHECK_DISTRIBUTION(axis.azimuth_dist, DistributionType::kUniform, 0, 360);
 }
 
 TEST_F(V3TestJson, Filter_None) {
