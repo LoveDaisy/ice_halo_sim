26edca3a04445af8f597f49339d656fa16a5525c
github-action-benchmark <github@users.noreply.github.com>
2026-04-05 16:47:37 +0000

    add Parallel Efficiency (customBiggerIsBetter) benchmark result for 8d7f4e2fda303a7cc2a7af7f40b3b510ee6c1c9f

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1775407656355,
+  "lastUpdate": 1775407657364,
   "repoUrl": "https://github.com/LoveDaisy/ice_halo_sim",
   "entries": {
     "Single-worker Throughput": [
@@ -98,6 +98,52 @@ window.BENCHMARK_DATA = {
             "value": 1046579.5,
             "unit": "rays/sec",
             "extra": "CPU: AMD EPYC 7763 64-Core Processor                \\nCores: 4"
+          }
+        ]
+      }
+    ],
+    "Parallel Efficiency": [
+      {
+        "commit": {
+          "author": {
+            "email": "zhangjiajie043@gmail.com",
+            "name": "Jiajie Zhang",
+            "username": "LoveDaisy"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "8d7f4e2fda303a7cc2a7af7f40b3b510ee6c1c9f",
+          "message": "Merge pull request #48 from LoveDaisy/feat/refactor_logger\n\nrefactor(core): remove LUMICE_InitLogger, decouple logger from server lifecycle",
+          "timestamp": "2026-04-06T00:41:43+08:00",
+          "tree_id": "b7ee785ca4ba9c280432d5769395837a5f16a3df",
+          "url": "https://github.com/LoveDaisy/ice_halo_sim/commit/8d7f4e2fda303a7cc2a7af7f40b3b510ee6c1c9f"
+        },
+        "date": 1775407657086,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "macOS ARM64",
+            "value": 94,
+            "unit": "%"
+          },
+          {
+            "name": "Ubuntu ARM64",
+            "value": 80.5,
+            "unit": "%"
+          },
+          {
+            "name": "Ubuntu x86_64",
+            "value": 71.8,
+            "unit": "%"
+          },
+          {
+            "name": "Windows MSVC x86_64",
+            "value": 68.9,
+            "unit": "%"
           }
         ]
       }
