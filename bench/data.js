588dfabbdca43acb141b8c0f5de15d8f1b790713
Jiajie Zhang <zhangjiajie043@gmail.com>
2026-04-05 20:19:45 +0800

    chore: remove old Benchmark group from data.js (keep 3-chart layout)

--- bench/data.js
+++ bench/data.js
@@ -2,100 +2,6 @@ window.BENCHMARK_DATA = {
   "lastUpdate": 1775391191772,
   "repoUrl": "https://github.com/LoveDaisy/ice_halo_sim",
   "entries": {
-    "Benchmark": [
-      {
-        "commit": {
-          "author": {
-            "email": "zhangjiajie043@gmail.com",
-            "name": "Jiajie Zhang",
-            "username": "LoveDaisy"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
-          "id": "936d10b478357be9e25274f14382698ead54d5e9",
-          "message": "Merge pull request #46 from LoveDaisy/feat/ci_bench_history\n\nfeat(ci): benchmark history dashboard on gh-pages",
-          "timestamp": "2026-04-05T19:44:13+08:00",
-          "tree_id": "eb2b9273349bd55fe135bdffa07d6be2b7eac7f7",
-          "url": "https://github.com/LoveDaisy/ice_halo_sim/commit/936d10b478357be9e25274f14382698ead54d5e9"
-        },
-        "date": 1775389661787,
-        "tool": "customBiggerIsBetter",
-        "benches": [
-          {
-            "name": "macOS ARM64 / Single",
-            "value": 447793.2,
-            "unit": "rays/sec",
-            "extra": "CPU: Apple M1 (Virtual)\\nCores: 3"
-          },
-          {
-            "name": "macOS ARM64 / Multi",
-            "value": 1217518.3,
-            "unit": "rays/sec",
-            "extra": "CPU: Apple M1 (Virtual)\\nCores: 3"
-          },
-          {
-            "name": "macOS ARM64 / Efficiency",
-            "value": 90.6,
-            "unit": "%"
-          },
-          {
-            "name": "Ubuntu ARM64 / Single",
-            "value": 627563.9,
-            "unit": "rays/sec",
-            "extra": "CPU: Neoverse-N2\\nCores: 4"
-          },
-          {
-            "name": "Ubuntu ARM64 / Multi",
-            "value": 1884722.5,
-            "unit": "rays/sec",
-            "extra": "CPU: Neoverse-N2\\nCores: 4"
-          },
-          {
-            "name": "Ubuntu ARM64 / Efficiency",
-            "value": 75.1,
-            "unit": "%"
-          },
-          {
-            "name": "Ubuntu x86_64 / Single",
-            "value": 423435.1,
-            "unit": "rays/sec",
-            "extra": "CPU: AMD EPYC 7763 64-Core Processor\\nCores: 4"
-          },
-          {
-            "name": "Ubuntu x86_64 / Multi",
-            "value": 1227378.8,
-            "unit": "rays/sec",
-            "extra": "CPU: AMD EPYC 7763 64-Core Processor\\nCores: 4"
-          },
-          {
-            "name": "Ubuntu x86_64 / Efficiency",
-            "value": 72.5,
-            "unit": "%"
-          },
-          {
-            "name": "Windows MSVC x86_64 / Single",
-            "value": 373396.6,
-            "unit": "rays/sec",
-            "extra": "CPU: AMD EPYC 7763 64-Core Processor                \\nCores: 4"
-          },
-          {
-            "name": "Windows MSVC x86_64 / Multi",
-            "value": 1054167.9,
-            "unit": "rays/sec",
-            "extra": "CPU: AMD EPYC 7763 64-Core Processor                \\nCores: 4"
-          },
-          {
-            "name": "Windows MSVC x86_64 / Efficiency",
-            "value": 70.6,
-            "unit": "%"
-          }
-        ]
-      }
-    ],
     "Single-worker Throughput": [
       {
         "commit": {
@@ -243,4 +149,4 @@ window.BENCHMARK_DATA = {
       }
     ]
   }
-}
+};
