c4530d56ebbd944c2b1d0dcf50fc7a8bd1349c26
github-action-benchmark <github@users.noreply.github.com>
2026-04-12 13:57:30 +0000

    add Multi-worker Throughput (customBiggerIsBetter) benchmark result for 12a30449eb7e798bdfa1f470ae2212f6aa518ead

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1776002248360,
+  "lastUpdate": 1776002250586,
   "repoUrl": "https://github.com/LoveDaisy/ice_halo_sim",
   "entries": {
     "Single-worker Throughput": [
@@ -1776,6 +1776,54 @@ window.BENCHMARK_DATA = {
           {
             "name": "Windows MSVC x86_64",
             "value": 1044003.2,
+            "unit": "rays/sec",
+            "extra": "CPU: AMD EPYC 7763 64-Core Processor                \\nCores: 4"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "zhangjiajie043@gmail.com",
+            "name": "Jiajie Zhang",
+            "username": "LoveDaisy"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "12a30449eb7e798bdfa1f470ae2212f6aa518ead",
+          "message": "Merge pull request #66 from LoveDaisy/feat/test_org\n\nReorganize test files: split GUI tests and separate integration tests",
+          "timestamp": "2026-04-12T21:53:12+08:00",
+          "tree_id": "d528ebc7c90a4396f461148ebfd75a5ede8a3e47",
+          "url": "https://github.com/LoveDaisy/ice_halo_sim/commit/12a30449eb7e798bdfa1f470ae2212f6aa518ead"
+        },
+        "date": 1776002250125,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "macOS ARM64",
+            "value": 1154292.4,
+            "unit": "rays/sec",
+            "extra": "CPU: Apple M1 (Virtual)\\nCores: 3"
+          },
+          {
+            "name": "Ubuntu ARM64",
+            "value": 1996745.6,
+            "unit": "rays/sec",
+            "extra": "CPU: Neoverse-N2\\nCores: 4"
+          },
+          {
+            "name": "Ubuntu x86_64",
+            "value": 1241531.3,
+            "unit": "rays/sec",
+            "extra": "CPU: AMD EPYC 7763 64-Core Processor\\nCores: 4"
+          },
+          {
+            "name": "Windows MSVC x86_64",
+            "value": 1054703.4,
             "unit": "rays/sec",
             "extra": "CPU: AMD EPYC 7763 64-Core Processor                \\nCores: 4"
           }
