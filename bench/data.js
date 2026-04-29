5cd8fd3d8bc68c10146a8344d5bb359fc76d8133
github-action-benchmark <github@users.noreply.github.com>
2026-04-29 07:59:20 +0000

    add Single-worker Throughput (customBiggerIsBetter) benchmark result for 26e54569e59d8e18420bf4a3a58d92d268b91b5a

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1777402708645,
+  "lastUpdate": 1777449560038,
   "repoUrl": "https://github.com/LoveDaisy/ice_halo_sim",
   "entries": {
     "Single-worker Throughput": [
@@ -1582,6 +1582,54 @@ window.BENCHMARK_DATA = {
           {
             "name": "Windows MSVC x86_64",
             "value": 374492.3,
+            "unit": "rays/sec",
+            "extra": "CPU: AMD EPYC 7763 64-Core Processor                \\nCores: 4"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "zhangjiajie043@gmail.com",
+            "name": "Jiajie Zhang",
+            "username": "LoveDaisy"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "26e54569e59d8e18420bf4a3a58d92d268b91b5a",
+          "message": "Merge pull request #81 from LoveDaisy/fix/modal_zorder\n\nfix(gui): show combo popups above detached Edit Entry modal",
+          "timestamp": "2026-04-29T15:54:43+08:00",
+          "tree_id": "6e2ad77922fffeadb6e2da7e47e4fa9f66c34300",
+          "url": "https://github.com/LoveDaisy/ice_halo_sim/commit/26e54569e59d8e18420bf4a3a58d92d268b91b5a"
+        },
+        "date": 1777449559582,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "macOS ARM64",
+            "value": 429091.7,
+            "unit": "rays/sec",
+            "extra": "CPU: Apple M1 (Virtual)\\nCores: 3"
+          },
+          {
+            "name": "Ubuntu ARM64",
+            "value": 608289.4,
+            "unit": "rays/sec",
+            "extra": "CPU: Neoverse-N2\\nCores: 4"
+          },
+          {
+            "name": "Ubuntu x86_64",
+            "value": 432789.5,
+            "unit": "rays/sec",
+            "extra": "CPU: AMD EPYC 7763 64-Core Processor\\nCores: 4"
+          },
+          {
+            "name": "Windows MSVC x86_64",
+            "value": 376391.8,
             "unit": "rays/sec",
             "extra": "CPU: AMD EPYC 7763 64-Core Processor                \\nCores: 4"
           }
