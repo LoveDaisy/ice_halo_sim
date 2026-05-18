fb41af107e9ade970799851bc820cae7ea65005e
github-action-benchmark <github@users.noreply.github.com>
2026-05-18 00:58:10 +0000

    add Single-worker Throughput (customBiggerIsBetter) benchmark result for 280bf0be7058816e808ba6a78cc98a7a5452eb6b

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1778761404735,
+  "lastUpdate": 1779065890332,
   "repoUrl": "https://github.com/LoveDaisy/ice_halo_sim",
   "entries": {
     "Single-worker Throughput": [
@@ -2112,6 +2112,54 @@ window.BENCHMARK_DATA = {
             "value": 441319.6,
             "unit": "rays/sec",
             "extra": "CPU: Intel(R) Xeon(R) Platinum 8370C CPU @ 2.80GHz\\nCores: 4"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "zhangjiajie043@gmail.com",
+            "name": "Jiajie Zhang",
+            "username": "LoveDaisy"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "280bf0be7058816e808ba6a78cc98a7a5452eb6b",
+          "message": "Merge pull request #92 from LoveDaisy/fix/raypath-1-3\n\nfix(core): repair raypath=[1,3] TIR edge leak via fid_in_src parameter",
+          "timestamp": "2026-05-18T08:53:02+08:00",
+          "tree_id": "8587ee20db4172496e5a7a3bd75b60244f2d8958",
+          "url": "https://github.com/LoveDaisy/ice_halo_sim/commit/280bf0be7058816e808ba6a78cc98a7a5452eb6b"
+        },
+        "date": 1779065889770,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "macOS ARM64",
+            "value": 432606.3,
+            "unit": "rays/sec",
+            "extra": "CPU: Apple M1 (Virtual)\\nCores: 3"
+          },
+          {
+            "name": "Ubuntu ARM64",
+            "value": 608202.4,
+            "unit": "rays/sec",
+            "extra": "CPU: Neoverse-N2\\nCores: 4"
+          },
+          {
+            "name": "Ubuntu x86_64",
+            "value": 496619.6,
+            "unit": "rays/sec",
+            "extra": "CPU: Intel(R) Xeon(R) Platinum 8370C CPU @ 2.80GHz\\nCores: 4"
+          },
+          {
+            "name": "Windows MSVC x86_64",
+            "value": 373682.2,
+            "unit": "rays/sec",
+            "extra": "CPU: AMD EPYC 7763 64-Core Processor                \\nCores: 4"
           }
         ]
       }
