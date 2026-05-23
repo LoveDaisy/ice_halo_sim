f4793a0f39bace810216af5102418f3d22190083
github-action-benchmark <github@users.noreply.github.com>
2026-05-23 09:46:53 +0000

    add Single-worker Throughput (customBiggerIsBetter) benchmark result for d158a9394600b1ded192e6dc974603f42b76fb47

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1779339327651,
+  "lastUpdate": 1779529613365,
   "repoUrl": "https://github.com/LoveDaisy/ice_halo_sim",
   "entries": {
     "Single-worker Throughput": [
@@ -2640,6 +2640,54 @@ window.BENCHMARK_DATA = {
             "value": 313412.4,
             "unit": "rays/sec",
             "extra": "CPU: AMD EPYC 9V74 80-Core Processor                \\nCores: 4"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "zhangjiajie043@gmail.com",
+            "name": "Jiajie Zhang",
+            "username": "LoveDaisy"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "d158a9394600b1ded192e6dc974603f42b76fb47",
+          "message": "Merge pull request #103 from LoveDaisy/feat/gui_card_link\n\nfeat(gui): linked entries via ID-pool model + pick-mode UX",
+          "timestamp": "2026-05-23T17:41:59+08:00",
+          "tree_id": "dcb404af64a0c4302dbf917a45e0f4888b230422",
+          "url": "https://github.com/LoveDaisy/ice_halo_sim/commit/d158a9394600b1ded192e6dc974603f42b76fb47"
+        },
+        "date": 1779529612768,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "macOS ARM64",
+            "value": 417999,
+            "unit": "rays/sec",
+            "extra": "CPU: Apple M1 (Virtual)\\nCores: 3"
+          },
+          {
+            "name": "Ubuntu ARM64",
+            "value": 627438.5,
+            "unit": "rays/sec",
+            "extra": "CPU: Neoverse-N2\\nCores: 4"
+          },
+          {
+            "name": "Ubuntu x86_64",
+            "value": 423504.7,
+            "unit": "rays/sec",
+            "extra": "CPU: AMD EPYC 7763 64-Core Processor\\nCores: 4"
+          },
+          {
+            "name": "Windows MSVC x86_64",
+            "value": 373722.3,
+            "unit": "rays/sec",
+            "extra": "CPU: AMD EPYC 7763 64-Core Processor                \\nCores: 4"
           }
         ]
       }
