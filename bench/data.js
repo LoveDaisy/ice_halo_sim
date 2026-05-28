1fc1ef22a9933f9bf433ef0ad5149e0a8245e08b
github-action-benchmark <github@users.noreply.github.com>
2026-05-28 07:43:18 +0000

    add Multi-worker Throughput (customBiggerIsBetter) benchmark result for e3d86034ece73c9d5b32a9981fc5b324bdbfbce6

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1779954196509,
+  "lastUpdate": 1779954198942,
   "repoUrl": "https://github.com/LoveDaisy/ice_halo_sim",
   "entries": {
     "Single-worker Throughput": [
@@ -6480,6 +6480,54 @@ window.BENCHMARK_DATA = {
           {
             "name": "Windows MSVC x86_64",
             "value": 869829,
+            "unit": "rays/sec",
+            "extra": "CPU: AMD EPYC 7763 64-Core Processor                \\nCores: 4"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "zhangjiajie043@gmail.com",
+            "name": "Jiajie Zhang",
+            "username": "LoveDaisy"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "e3d86034ece73c9d5b32a9981fc5b324bdbfbce6",
+          "message": "Merge pull request #115 from LoveDaisy/feat/remove_ev_anchor\n\nRemove F1 anchor lane, use self-P99.5 EV normalization",
+          "timestamp": "2026-05-28T15:38:16+08:00",
+          "tree_id": "52693d70e631b7a71599cdcad16ec33dd9f6c0d1",
+          "url": "https://github.com/LoveDaisy/ice_halo_sim/commit/e3d86034ece73c9d5b32a9981fc5b324bdbfbce6"
+        },
+        "date": 1779954198607,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "macOS ARM64",
+            "value": 1145463.7,
+            "unit": "rays/sec",
+            "extra": "CPU: Apple M1 (Virtual)\\nCores: 3"
+          },
+          {
+            "name": "Ubuntu ARM64",
+            "value": 1886299.5,
+            "unit": "rays/sec",
+            "extra": "CPU: Neoverse-N2\\nCores: 4"
+          },
+          {
+            "name": "Ubuntu x86_64",
+            "value": 1068044.4,
+            "unit": "rays/sec",
+            "extra": "CPU: AMD EPYC 9V74 80-Core Processor\\nCores: 4"
+          },
+          {
+            "name": "Windows MSVC x86_64",
+            "value": 936650.1,
             "unit": "rays/sec",
             "extra": "CPU: AMD EPYC 7763 64-Core Processor                \\nCores: 4"
           }
