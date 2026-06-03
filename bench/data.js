b8a05a8b57b2cb24b486ebcf8bec0208a1bf3bcf
github-action-benchmark <github@users.noreply.github.com>
2026-06-03 10:11:06 +0000

    add Single-worker Throughput (customBiggerIsBetter) benchmark result for 392ee041c7255f883dcb83f6eb4c4958ea8f8d28

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1780382994359,
+  "lastUpdate": 1780481466957,
   "repoUrl": "https://github.com/LoveDaisy/ice_halo_sim",
   "entries": {
     "Single-worker Throughput": [
@@ -3456,6 +3456,54 @@ window.BENCHMARK_DATA = {
             "value": 417429.8,
             "unit": "rays/sec",
             "extra": "CPU: Intel(R) Xeon(R) Platinum 8370C CPU @ 2.80GHz\\nCores: 4"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "zhangjiajie043@gmail.com",
+            "name": "Jiajie Zhang",
+            "username": "LoveDaisy"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "392ee041c7255f883dcb83f6eb4c4958ea8f8d28",
+          "message": "Merge pull request #120 from LoveDaisy/dev/perf_opt2\n\nperf(worker-default): default worker count to physical cores",
+          "timestamp": "2026-06-03T18:05:54+08:00",
+          "tree_id": "0863f2e30f743b4cf8bdb1004f290e0da8e43762",
+          "url": "https://github.com/LoveDaisy/ice_halo_sim/commit/392ee041c7255f883dcb83f6eb4c4958ea8f8d28"
+        },
+        "date": 1780481465771,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "macOS ARM64",
+            "value": 381846,
+            "unit": "rays/sec",
+            "extra": "CPU: Apple M1 (Virtual)\\nCores: 3"
+          },
+          {
+            "name": "Ubuntu ARM64",
+            "value": 608568.4,
+            "unit": "rays/sec",
+            "extra": "CPU: Neoverse-N2\\nCores: 4"
+          },
+          {
+            "name": "Ubuntu x86_64",
+            "value": 398177.7,
+            "unit": "rays/sec",
+            "extra": "CPU: AMD EPYC 7763 64-Core Processor\\nCores: 4"
+          },
+          {
+            "name": "Windows MSVC x86_64",
+            "value": 322210.3,
+            "unit": "rays/sec",
+            "extra": "CPU: AMD EPYC 9V74 80-Core Processor                \\nCores: 4"
           }
         ]
       }
